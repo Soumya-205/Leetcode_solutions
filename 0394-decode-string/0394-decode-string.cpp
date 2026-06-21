7e7e67bd2b60f354ec0ac8ee2f90bb6081ac7e49
Soumya Shradha <soumyashradha449@gmail.com>
2026-06-21 22:15:48 +0530

    Time: 2 ms (23.93%), Space: 9.7 MB (17.07%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int>countStack;
+        stack<string>stringStack;
+
+        int num=0;
+        string currStr="";
+
+        for(char ch:s){
+            if(isdigit(ch)){
+                num=num*10+(ch-'0');
+            }
+            else if(ch=='['){
+                countStack.push(num);
+                stringStack.push(currStr);
+
+                num=0;
+                currStr="";
+            }
+            else if(ch==']'){
+                int repeat=countStack.top();
+                countStack.pop();
+
+                string prevStr= stringStack.top();
+                stringStack.pop();
+
+                string temp="";
+                for(int i=0;i<repeat;i++){
+                    temp+=currStr;
+                }
+                currStr=prevStr+temp;
+            }
+            else{
+                currStr+=ch;
+            }
+        }
+        return currStr;
+    }
+};
